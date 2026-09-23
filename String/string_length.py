440b389f0fa9815b92ade76d66cb9b3159412a69
Abdul_Aziz <abdulnsec01@gmail.com>
2026-09-23 16:18:05 +0530

    string folder and string length function added

---
+++ String/string_length.py
@@ -0,0 +1,14 @@
+def str_len(s):
+    """
+    Returns the length of the given string.
+
+    Args:
+        s (str): The input string.
+
+    Returns:
+        int: The length of the string.
+    """
+    count = 0
+    for char in s:
+        count += 1
+    return count
